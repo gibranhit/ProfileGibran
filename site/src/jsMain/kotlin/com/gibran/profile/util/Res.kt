01f1c384d62a3e1bb2ee1f486b5d2672b830e420
gibranhit <gibrannhit@gmail.com>
2025-04-29 14:22:52 -0600

    finish profile

---
+++ site/src/jsMain/kotlin/com/gibran/profile/util/Res.kt
@@ -0,0 +1,54 @@
+package com.stevdza_san.website.util
+
+import com.varabyte.kobweb.compose.ui.graphics.Color
+
+object Res {
+    enum class Theme(val color: Color) {
+        BLUE(color = Color.rgb(r = 33, g = 150, b = 243)),
+        LIGHT_BLUE(color = Color.rgb(r = 168, g = 236, b = 255)),
+        DARK_BLUE(color = Color.rgb(r = 34, g = 63, b = 94)),
+        SOCIAL_ICON_BACKGROUND_LIGHT(color = Color.rgb(r = 230, g = 230, b = 230)),
+        SOCIAL_ICON_BACKGROUND_DARK(color = Color.rgb(r = 48, g = 82, b = 118)),
+        GRADIENT_ONE(color = Color.rgb(r = 161, g = 196, b = 253)),
+        GRADIENT_ONE_DARK(color = Color.rgb(r = 19, g = 38, b = 58)),
+        GRADIENT_TWO(color = Color.rgb(r = 194, g = 233, b = 251)),
+        GRADIENT_TWO_DARK(color = Color.rgb(r = 20, g = 46, b = 73))
+    }
+
+    object Icon {
+        const val EMAIL_LIGHT = "mail.svg"
+        const val EMAIL_DARK = "mail_light.svg"
+        const val GITHUB = "github.svg"
+        const val GITHUB_LIGHT = "github_light.svg"
+        const val INSTAGRAM = "instagram.svg"
+        const val INSTAGRAM_LIGHT = "instagram_light.svg"
+        const val GITLAB = "gitlab.svg"
+        const val GITLAB_LIGHT = "gitlab_light.svg"
+        const val SUN = "sun.svg"
+        const val MOON = "moon.svg"
+    }
+
+    object Image {
+        const val PROFILE_PHOTO = "photo.jpg"
+    }
+
+    object String {
+        const val NAME = "Gibran Reyes"
+        const val PROFESSION = "Senior Android Engineer/Kotlin Multiplaform Engineer"
+        const val ABOUT_ME =
+            "I’m a Senior Android Engineer with 7+ years of experience specializing in the financial sector. Skilled in Kotlin, Jetpack Compose, MVVM, and Clean Architecture, I design and build scalable mobile solutions that drive business success. Passionate about crafting seamless user experiences and optimizing complex workflows."
+        const val BUTTON_TEXT = "Let’s build something great together!"
+        const val ROBOTO_CONDENSED = "RobotoCondensedBold"
+        const val ROBOTO_REGULAR = "RobotoRegular"
+        const val MY_EMAIL = "mailto:gibrannhit@gmail.com"
+        const val SAVED_THEME = "theme"
+    }
+
+    object Dimens {
+        const val BORDER_RADIUS = 8
+        const val MAX_CARD_WIDTH = 1000
+        const val MAX_CARD_HEIGHT = 600
+        const val ICON_SIZE = 24
+        const val ICON_SIZE_LG = 32
+    }
+}
